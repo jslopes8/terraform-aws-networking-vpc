ccce4224fe3ddc650d036b01f8e61286c7c61b19
jslopes <jslopes@uoldiveo.com>
2020-09-18 17:12:41 -0300

    suport para auto aceito

--- main.tf
+++ main.tf
@@ -451,21 +451,19 @@ EOF
 resource "aws_vpc_peering_connection" "main" {
     count   = var.create ? length(var.vpc_peering_connection) : 0
 
-	depends_on = [ aws_vpc.main ]
+    depends_on = [ aws_vpc.main ]
 
-  	peer_vpc_id = aws_vpc.main.0.id
-  	vpc_id      = lookup(var.vpc_peering_connection[count.index], "accepter_vpc_id", null)
-  	peer_region = lookup(var.vpc_peering_connection[count.index], "accepter_vpc_region", null)
-    auto_accept = lookup(var.vpc_peering_connection[count.index], "auto_accept", null )
+    peer_vpc_id = lookup(var.vpc_peering_connection[count.index], "accepter_vpc_id", null)
+    vpc_id      = aws_vpc.main.0.id
+    peer_region = lookup(var.vpc_peering_connection[count.index], "accepter_vpc_region", null)
 
-	tags = var.default_tags
+    tags    = var.default_tags
 }
-resource "aws_route" "rt_peering" {
+resource "aws_vpc_peering_connection_accepter" "main" {
     count   = var.create ? length(var.vpc_peering_connection) : 0
 
-    route_table_id            = aws_route_table.private.0.id
-    destination_cidr_block    = lookup(var.vpc_peering_connection[count.index], "destination_cidr_block", null)
     vpc_peering_connection_id = aws_vpc_peering_connection.main.0.id
+    auto_accept               = lookup(var.vpc_peering_connection[count.index], "auto_accept", null )
 
-    depends_on                = [ aws_route_table.public, aws_vpc_peering_connection.main, aws_vpc.main ]
+    tags    = var.default_tags
 }
